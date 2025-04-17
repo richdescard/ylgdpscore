7baa6e8e48fd9fae6719d5148e3f31ca81f47fff
richdescard <richdescard@gmail.com>
2025-04-17 16:14:45 -0300

    Update dailyChests.php
    made stats way lower coz its too high for any regu lar gdp´s eeo rsomething

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -7,21 +7,21 @@
 	REWARDS
 */
 //SMALL CHEST
-$chest1minOrbs = 200;
-$chest1maxOrbs = 400;
-$chest1minDiamonds = 2;
-$chest1maxDiamonds = 10;
+$chest1minOrbs = 100;
+$chest1maxOrbs = 250;
+$chest1minDiamonds = 0;
+$chest1maxDiamonds = 2;
 $chest1items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
-$chest1minKeys = 1;
-$chest1maxKeys = 6;
+$chest1minKeys = 0;
+$chest1maxKeys = 3;
 //BIG CHEST
-$chest2minOrbs = 2000;
-$chest2maxOrbs = 4000;
-$chest2minDiamonds = 20;
-$chest2maxDiamonds = 100;
+$chest2minOrbs = 500;
+$chest2maxOrbs = 2569;
+$chest2minDiamonds = 1;
+$chest2maxDiamonds = 5;
 $chest2items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
 $chest2minKeys = 1;
-$chest2maxKeys = 6;
+$chest2maxKeys = 3;
 //REWARD TIMES (in seconds)
 $chest1wait = 3600;
 $chest2wait = 14400;
